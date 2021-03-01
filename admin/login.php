6e79f56e496095050fe18f74f787965c7fb460cf
TOFAYEL H TOPU <tofayelhtopu@gmail.com>
2021-03-02 01:01:42 +0600

    start on login system

--- admin/login.php
+++ admin/login.php
@@ -1,13 +1,22 @@
+<?php include '../classes/AdminLogin.php'?>
+<?php
+$admin_login = new AdminLogin();
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+    $check_login = $admin_login->adminLogin($user_name, $password);
+}
+?>
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
-<title>Login</title>
+<title>Admin Login</title>
     <link rel="stylesheet" type="text/css" href="css/stylelogin.css" media="screen" />
 </head>
 <body>
 <div class="container">
 	<section id="content">
-		<form action="" method="post">
+		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
 			<div>
 				<input type="text" placeholder="Username" required="" name="username"/>
@@ -19,9 +28,6 @@
 				<input type="submit" value="Log in" />
 			</div>
 		</form><!-- form -->
-		<div class="button">
-			<a href="#">Training with live project</a>
-		</div><!-- button -->
 	</section><!-- content -->
 </div><!-- container -->
 </body>
---
+++ classes/AdminLogin.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class AdminLogin
+{
+    public function __construct(){
+
+    }
+    public function adminLogin(){
+
+    }
+
+}
