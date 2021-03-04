ec1261a6a1671d5c108cb6eda457bac9f2195445
TOFAYEL H TOPU <tofayelhtopu@gmail.com>
2021-03-05 01:22:46 +0600

    login and logout

--- admin/login.php
+++ admin/login.php
@@ -5,6 +5,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $user_name = $_POST['user_name'];
     $password = $_POST['password'];
     $check_login = $admin_login->adminLogin($user_name, $password);
+//    var_dump($check_login);
 }
 ?>
 <!DOCTYPE html>
@@ -18,11 +19,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	<section id="content">
 		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
+            <span style="color: #ff0000;font-size: 18px">
+                <?php
+                if (isset($check_login)){
+                    echo $check_login;
+                }
+                ?>
+            </span>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" placeholder="Enter User Name" required name="user_name"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" placeholder="Enter Password" required name="password"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
--- classes/AdminLogin.php
+++ classes/AdminLogin.php
@@ -1,13 +1,44 @@
 <?php
+include '../lib/Session.php';
+Session::checkLogin();
+include_once '../lib/Database.php';
+include_once '../helpers/Format.php';
+?>
+<?php
 
 
 class AdminLogin
 {
+    private $db;
+    private $fm;
     public function __construct(){
-
+        $this->db = new Database();
+        $this->fm = new Format();
     }
-    public function adminLogin(){
-
+    public function adminLogin($user_name, $password){
+        $name = $this->fm->validation($user_name);
+        $password = $this->fm->validation($password);
+        $name = mysqli_real_escape_string($this->db->link, $name);
+        $password = mysqli_real_escape_string($this->db->link, $password);
+        if (empty($name) || empty($password)) {
+            $msg = "User or password must not be empty!";
+            return $msg;
+        }else {
+            $query = "SELECT * FROM users WHERE user_name = '$name' AND password = '$password'";
+            $result = $this->db->select($query);
+//            var_dump($result);
+            if ($result != false){
+                $value = $result->fetch_assoc();
+                Session::set("login", true);
+                Session::set("user_id", $value['id']);
+                Session::set("user_name", $value['user_name']);
+                Session::set("full_name", $value['full_name']);
+                header('Location:dashboard.php');
+            } else {
+                $msg = "User name or password not match!";
+                return $msg;
+            }
+        }
     }
 
 }
